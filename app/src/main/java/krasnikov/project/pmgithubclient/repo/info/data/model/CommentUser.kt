8b26226bd9df32dfe7cea5007c053e961c58194d
a-krasnikov <andriikrasnikov@gmail.com>
2021-02-27 15:26:14 +0200

    remove CommentUser

--- app/src/main/java/krasnikov/project/pmgithubclient/repo/info/data/model/CommentUser.kt
+++
@@ -1,7 +0,0 @@
-package krasnikov.project.pmgithubclient.repo.info.data.model
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class CommentUser(@SerialName("login") val login: String)
--- app/src/main/java/krasnikov/project/pmgithubclient/repo/issue/data/model/Comment.kt
+++ app/src/main/java/krasnikov/project/pmgithubclient/repo/issue/data/model/Comment.kt
@@ -2,12 +2,12 @@ package krasnikov.project.pmgithubclient.repo.issue.data.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import krasnikov.project.pmgithubclient.repo.info.data.model.CommentUser
+import krasnikov.project.pmgithubclient.userinfo.data.model.User
 
 @Serializable
 data class Comment(
     @SerialName("id") val id: Int,
     @SerialName("body") val body: String,
-    @SerialName("user") val user: CommentUser,
+    @SerialName("user") val user: User,
     @SerialName("issue_url") val issueUrl: String
 )
