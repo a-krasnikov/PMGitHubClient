2ec82fe1327b7356a6a51e4ae052034085a19aa0
a-krasnikov <andriikrasnikov@gmail.com>
2021-02-27 14:17:48 +0200

    Utils fix

--- app/src/main/java/krasnikov/project/pmgithubclient/utils/Error.kt
+++ app/src/main/java/krasnikov/project/pmgithubclient/utils/Error.kt
@@ -1,12 +1,13 @@
 package krasnikov.project.pmgithubclient.utils
 
 import androidx.annotation.StringRes
+import krasnikov.project.pmgithubclient.R
 
-enum class ErrorType(@StringRes val stringRes: Int = 0) {
-    NetworkError,
-    IssueCommentsError,
-    AccessTokenError,
-    ReposNotLoadedError,
-    UserNotLoadedError,
-    UnknownError
+enum class ErrorType(@StringRes val stringRes: Int) {
+    NetworkError(R.string.error_network),
+    IssueCommentsError(R.string.error_issue_comments),
+    AccessTokenError(R.string.error_access_token),
+    ReposNotLoadedError(R.string.error_repos_not_loaded),
+    UserNotLoadedError(R.string.error_user_not_loaded),
+    UnknownError(R.string.error_unknown)
 }
